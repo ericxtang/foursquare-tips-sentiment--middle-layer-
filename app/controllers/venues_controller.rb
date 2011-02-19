8c0b61993e9d555a03d4880ce4721f8c1602a5e3
ericxtang <etang@hyperpublic.com>
2011-02-19 16:44:15 -0500

    adding callback function

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,39 +7,24 @@ require 'ruby-debug'
 
 class VenuesController < ApplicationController
   def index
-    fs = Foursquare::Venue.new("T4ZOBBXF3AK522ZJHAWZTGELNNFUSQF4BC4HA4XLWJEAVZWD")
-    result = fs.search(:ll => "#{params[:lat]},#{params[:lon]}",:query => params[:q])["response"]
-    venues = []
-    result["groups"].each do |group|
-      venues += group["items"]
-    end
-    venues.sort! {|a, b| b["stats"]["checkinsCount"] <=> a["stats"]["checkinsCount"]}
-    venues = venues.first(10)
-    venue_ids = venues.collect do |v|
-      v["id"]
-    end
-    scores = fetch_scores(venue_ids)
-    result = scores.zip(venues).collect do |tuple|
-      {
-        :venue_id => tuple[0]["venue_id"],
-        :name => tuple[1]["name"],
-        :lat => tuple[1]["location"]["lat"],
-        :lon => tuple[1]["location"]["lng"],
-        :scores => tuple[0]["scores"]
-      }
-    end
-    result.to_json
   end
 
   def show
+    if params[:lat].blank? || params[:lon].blank? || params[:query].blank?
+      render :json => {:errors => "Need to pass in a lat and a lon"} and return
+    end
     fs = Foursquare::Venue.new("T4ZOBBXF3AK522ZJHAWZTGELNNFUSQF4BC4HA4XLWJEAVZWD")
     result = fs.search(:ll => "#{params[:lat]},#{params[:lon]}",:query => params[:q])["response"]
     venues = []
     result["groups"].each do |group|
       venues += group["items"]
     end
+
     venues.sort! {|a, b| b["stats"]["checkinsCount"] <=> a["stats"]["checkinsCount"]}
-    venues = venues.first(10)
+    venues.collect! do |v|
+      v if v["stats"]["checkinsCount"] > 10
+    end.compact!
+
     venue_ids = venues.collect do |v|
       v["id"]
     end
@@ -53,11 +38,10 @@ class VenuesController < ApplicationController
         :scores => tuple[0]["scores"]
       }
     end
-    render :json => result
+    render :text => "#{params[:callback]}(#{result.to_json});", :status => 200
   end
 
   def fetch_scores(venue_ids)
-    #url = "http://#{venue_ids.join(",")}"
     url = "http://foursquare.xanthas.net/pull_tips.php?#{venue_ids.join(",")}"
     results = JSON.parse(Net::HTTP.get(URI.parse(url)))
   end
